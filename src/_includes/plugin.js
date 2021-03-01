ced2596ee906c363b54a50b05e69105df9ef7568
Tixie <admin@glitch.family>
2021-03-01 04:03:23 +0100

    Add rel="noopener" to externalinks

--- src/_includes/plugin.js
+++ src/_includes/plugin.js
@@ -5,14 +5,14 @@ module.exports = function (plugin) {
     if (plugin.url_npm) { sources.push({ label: 'npm', url: plugin.url_npm }) }
 
     const links = sources.map(source => {
-        return `<a class="plugin__link plugin__link--${source.label}" href="${source.url}?ref=vanillalist" target="_blank">${source.label}</a>`
+        return `<a class="plugin__link plugin__link--${source.label}" href="${source.url}?ref=vanillalist" target="_blank" rel="noopener">${source.label}</a>`
     }).join('')
 
     const visualLink = plugin.url_demo ? plugin.url_demo : plugin.url_github ? plugin.url_github : plugin.url_npm ? plugin.url_npm : ''
 
     return `
         <div class="plugin">
-            <a class="plugin__visual" href="${visualLink}?ref=vanillalist">
+            <a class="plugin__visual" href="${visualLink}?ref=vanillalist" target="_blank" rel="noopener">
                 <img srcset="${plugin.image} 1x, ${plugin.image2x} 2x" alt="${plugin.title}" width="308" height="195">
             </a>
             <h2 class="plugin__name">${plugin.title}</h2>
