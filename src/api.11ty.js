07ac97761cbc1a58210b86d03b1fd547d9a6e2d7
Tixie <admin@glitch.family>
2022-02-10 06:00:39 +0100

    Serve thumbnails as webp

--- src/api.11ty.js
+++ src/api.11ty.js
@@ -9,10 +9,10 @@ class Main {
         const list = (await Promise.all(data.collections.plugins.map(async plugin => {
             return {
                 ...plugin,
-                image92: await this.getUrl(this.png(this.resize('./uploads/' + plugin.image, { width: 92, height: 58 }))),
-                image184: await this.getUrl(this.png(this.resize('./uploads/' + plugin.image, { width: 184, height: 116 }))),
-                image308: await this.getUrl(this.png(this.resize('./uploads/' + plugin.image, { width: 308, height: 195 }))),
-                image616: await this.getUrl(this.png(this.resize('./uploads/' + plugin.image, { width: 616, height: 390 })))
+                image92: await this.getUrl(this.webp(this.resize('./uploads/' + plugin.image, { width: 92, height: 58 }))),
+                image184: await this.getUrl(this.webp(this.resize('./uploads/' + plugin.image, { width: 184, height: 116 }))),
+                image308: await this.getUrl(this.webp(this.resize('./uploads/' + plugin.image, { width: 308, height: 195 }))),
+                image616: await this.getUrl(this.webp(this.resize('./uploads/' + plugin.image, { width: 616, height: 390 })))
             }
         })));
 
